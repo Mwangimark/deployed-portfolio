b5723a807123ec2be7efa66faf7787d2d4bfe510
Mwangimark <markcosmars2000@gmail.com>
2025-08-24 20:03:21 +0300

    linked ln

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ function Footer() {
     <footer>
       <div>
         <a href="https://github.com/Mwangimark" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-        <a href="https://www.linkedin.com/in/mark-mwangi-957591257/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+        <a href="hhttps://www.linkedin.com/in/mark-mwangi-957591257?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
       </div>
       <p>A portfolio designed & built by <a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer">Mwangi mark</a> with 💜</p>
     </footer>
